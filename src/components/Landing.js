bf13f445898c2ff68a24aa97c45054c48f919cce
Ryan Eldon <47878230+rdogstyle@users.noreply.github.com>
2022-05-18 13:47:46 -0400

    Test commit for production deployment

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,7 +5,7 @@ function Landing() {
 	return (
 		<div className="landing__container">
 			<div className="nameBox">
-				<h1>Ryan Eldon</h1>
+				<h1>Goodbye</h1>
 			</div>
 			<h2>Web and App Development Portfolio</h2>
 			<div className="contact__info">
